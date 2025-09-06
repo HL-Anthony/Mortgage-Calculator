f0ec7568dd82f89f56a08d443267c1ee59e5e6b3
Anthony <hansen.anthony1997@gmail.com>
2025-09-06 17:05:45 +0200

    hole code with some bugs

--- src/fullCode/MainClass.java
+++ src/fullCode/MainClass.java
@@ -1,21 +1,25 @@
 package fullCode;
 import javax.swing.*;
+import java.text.NumberFormat;
 
 public class MainClass {
 
 
 // Declaring the variables for the user input
-static float loanTotal;
+static float loan;
 static int years;
 static float interestRate;
 
+
+  //----------------------------------------------------------------------------------------------------------------//
 
 
     public static void main (String [] args) {
 
         JOptionPane.showMessageDialog(null, "Thank you for using my mortgage calculator :)");
         getInput();
-
+        calculations();
+        printResults();
 
     }
 
@@ -27,7 +31,7 @@ static float interestRate;
 
         try{
 
-            loanTotal = Float.parseFloat(JOptionPane.showInputDialog("How much do you want to borrow ?"));
+            loan = Float.parseFloat(JOptionPane.showInputDialog("How much do you want to borrow ?"));
             years = Integer.parseInt(JOptionPane.showInputDialog("How many years do you want to spread the payments across ?"));
             interestRate = Float.parseFloat(JOptionPane.showInputDialog("what is your annual interest rate ?"));
 
@@ -42,15 +46,39 @@ static float interestRate;
 
 
 // Declaring the necessary variables for the calculations
-static int numOfPayments = years * 12;
-static float decimalParentInterest = interestRate/100;
-static float monthlyInterestRate = decimalParentInterest/12;
 
+    static double monthlyPayments;
+    static double totalLoan;
 
 
     public static void calculations(){
 
+        float numOfPayments = years * 12;
+        float decimalParentInterest = interestRate/100;
+        float monthlyInterestRate = decimalParentInterest/12;
+        float onePlussInterest = 1 + monthlyInterestRate;
 
+
+        double numerator = (Math.pow(onePlussInterest, numOfPayments)) * monthlyInterestRate;
+        double denomerator = (numerator - 1);
+
+
+        monthlyPayments = loan * (numerator/denomerator);
+        totalLoan = monthlyPayments * 12 ;
+    }
+
+
+    public static void printResults() {
+
+
+        NumberFormat initLoan = NumberFormat.getCurrencyInstance();
+
+        String loanFormat =  initLoan.format(loan);
+        String monthlyPaymentsFormat = initLoan.format(monthlyPayments);
+        String totalLoanFormat = initLoan.format(totalLoan);
+
+        String paymentStats = "Loan amount : "+ loanFormat +"\n" + "Interest rate : " + interestRate + "\n\n" + "Monthly payments : " + monthlyPaymentsFormat + "\n" + "Total loan payment : " + totalLoanFormat ;
+        JOptionPane.showMessageDialog(null, "Borrowing " + loanFormat + " With an " + interestRate + " % interest rate wil give you : \n\n" + paymentStats);
 
 
     }
